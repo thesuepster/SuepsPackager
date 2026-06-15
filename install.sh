3eb9f284ddc315b63bee1ba906fb812fad17bc54
suep <thesuepster@gmail.com>
2026-06-14 23:13:24 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 echo "Installing spkg..."
-curl -fsSL https://raw.githubusercontent.com/thesuepster/Sueps-Packager/main/spkg -o /usr/local/bin/spkg
+curl -fsSL https://raw.githubusercontent.com/thesuepster/SuepsPackager/main/spkg -o /usr/local/bin/spkg
 chmod +x /usr/local/bin/spkg
 echo "Done! Try: sudo spkg search <app>"
